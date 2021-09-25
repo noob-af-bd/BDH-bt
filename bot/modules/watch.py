e39e3b349c6cbfdee4780ef170c69629cabe0e57
Alex Stuart <77671874+noob-af-bd@users.noreply.github.com>
2021-09-25 22:03:39 +0600

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,11 +17,11 @@ def _watch(bot: Bot, update, isTar=False, isZip=False, isLeech=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
-        msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
-        msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
-        msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
+        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] ইউটিউব ভিডিও মিরর করতে এইভাবে কমান্ড ব্যাবহার করুন.\n\n"
+        msg += "<b>নোট: কোয়ালিটি ও নতুন নাম না দিলেও চলবে</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
+        msg += "\n\nনতুন নাম দিতে চাইলে | এই চিহ্নের পরে দিন "
+        msg += f"\n\nউদাহরন:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthLeE 720 | TESTfile</code>\n\n"
+        msg += "এই ফাইলটি ৭২০পি তে ডাউনলোড হবে এবং ফাইলের নাম হবে <b>TESTfile</b>"
         sendMessage(msg, bot, update)
         return
     
