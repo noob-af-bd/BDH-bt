b9c296ca20c419814c7e407e553ddc7ecdaa6037
Alex Stuart <77671874+noob-af-bd@users.noreply.github.com>
2021-09-25 21:42:01 +0600

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -14,7 +14,7 @@ def countNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Counting: <code>{link}</code>", context.bot, update)
+        msg = sendMessage(f"সাইজ হিসাব করা হচ্ছে: <code>{link}</code>", context.bot, update)
         gd = GoogleDriveHelper()
         result = gd.count(link)
         deleteMessage(context.bot, msg)
@@ -23,10 +23,10 @@ def countNode(update, context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\n\ncc: {uname}'
+            cc = f'\n\n {uname} , এই হচ্ছে আপনার মোট ফাইল সাইজ'
         sendMessage(result + cc, context.bot, update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Count.", context.bot, update)
+        sendMessage("সাইজ হিসাব করার জন্য গুগল ড্রাইভের পাবলিক লিংক অথবা শেয়ার পারমিশন সহ লিংক প্রদান করুন.", context.bot, update)
 
 count_handler = CommandHandler(BotCommands.CountCommand, countNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(count_handler)
